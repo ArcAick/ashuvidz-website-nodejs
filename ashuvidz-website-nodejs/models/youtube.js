884307a8ee06157394c143b6e277fc957dec4a2c
VyrtualSynthese <vyrtualsynthese@gmail.com>
2017-12-30 02:00:03 +0100

    moving files up

--- ashuvidz-website-nodejs/models/youtube.js
+++
@@ -1,6 +0,0 @@
-var request = require('request-promise');
-
-module.exports = {
-    videoPageGenerator: function (){
-        return request('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUBofSSDlt4EZki2VQ9PWnyw&part=snippet&key=AIzaSyAy0qWFBAcoQfjjykI8O0njmofEF5nvoJY&maxResults=9');
-}};
